047e97567ec110e07ba99a90f2011d813f32438c
Jordan Bancino <jordan@bancino.net>
2019-09-17 20:21:28 -0400

    Create drive motors variable

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import java.util.HashMap;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -12,21 +15,28 @@ import frc.robot.RobotMap;
  */
 public class SwerveDrive extends Subsystem {
 
+  /* Encoder settings */
   private static final boolean invert = false;
   private static final EncodingType encodingType = EncodingType.k4X;
+
+  /* Motor settings */
+  public static final MotorType motorType = MotorType.kBrushless;
 
   public static enum Module {
     FRONT_LEFT, FRONT_RIGHT,
     REAR_LEFT, REAR_RIGHT
   }
 
-  private HashMap<Module, Encoder> encoders = new HashMap<Module, Encoder>();
+  private final HashMap<Module, Encoder> encoders = new HashMap<Module, Encoder>();
+  private final HashMap<Module, CANSparkMax> driveMotors = new HashMap<Module, CANSparkMax>();
   
   public SwerveDrive() {
     encoders.put(Module.FRONT_LEFT, new Encoder(0, RobotMap.FRONT_LEFT_ANALOG_ENCODER, invert, encodingType));
     encoders.put(Module.FRONT_RIGHT, new Encoder(0, RobotMap.FRONT_RIGHT_ANALOG_ENCODER, invert, encodingType));
     encoders.put(Module.REAR_LEFT, new Encoder(0, RobotMap.REAR_LEFT_ANALOG_ENCODER, invert, encodingType));
     encoders.put(Module.REAR_RIGHT, new Encoder(0, RobotMap.REAR_RIGHT_ANALOG_ENCODER, invert, encodingType));
+
+    
   }
 
   public HashMap<Module, Encoder> getEncoders() {
