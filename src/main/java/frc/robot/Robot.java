862ea62310f022e2ed5d7c249787598ce8480700
Jordan Bancino <jordan@bancino.net>
2019-10-05 09:08:47 -0400

    Make sure the drivetrain doesn't go wild on startup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,6 +31,7 @@ public class Robot extends TimedRobot {
     //chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", chooser);
+    Subsystems.driveTrain.stop();
   }
 
   /**
