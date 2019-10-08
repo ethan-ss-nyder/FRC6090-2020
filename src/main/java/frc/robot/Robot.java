e2b85c1b777fbfde35fc71b46e3ca964481f9e6d
Jordan Bancino <jordan@bancino.net>
2019-10-08 17:09:40 -0400

    Use the new OI.registerCommand() form

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.OI.ActionType;
-import frc.robot.commands.ZeroDrivetrainEncoders;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -37,7 +36,14 @@ public class Robot extends TimedRobot {
      * in the OI. This method allows the OI to be portable, that is, it can be used
      * across robot projects and years. 
      */
-    oi.registerCommand(11, ActionType.PRESS, new ZeroDrivetrainEncoders());
+
+    /* Zero the drivetrain encoders when button 11 is pressed. */
+    oi.registerCommand(11, ActionType.PRESS, new Runnable() {
+      @Override
+      public void run() {
+        Subsystems.driveTrain.zero();
+      }
+    }, Subsystems.driveTrain);
 
     //chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
